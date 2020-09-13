3c87d3b8ca40362e3533b9e10ae152aca702f26b
Rolmes <roomeeroobriittoo@gmail.com>
2020-09-13 16:38:33 -0300

    Nova tela para criação de salas

---
+++ salas/criar.php
@@ -0,0 +1,25 @@
+<div class="criacao-sala">
+    <h1>CRIAR SALA</h1>
+    <div></div>
+    <p><label>Nome da sala</label></p>
+    <input type="text" />
+    <div>
+    <p><label>Tema da partida</label></p>
+    <div>
+        <select>
+          <option value="0">Tema A</option>
+          <option value="1">Tema B</option>
+          <option value="2">Tema C</option>
+          <option value="3">Tema D</option>
+          <option value="4">Tema E</option>
+        </select>
+    </div>   
+    <p><label>Número de jogadores (Máximo 4)</label></p>
+    <input type="text" />   
+
+    <p><label>Senha (Não obrigatório)</label></p>
+    <input type="text" />   
+
+    <p><button type="submit">Criar</button> 
+    <button type="submit">Cancelar</button>
+</div>
