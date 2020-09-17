a750b65c898ebecaaf81ab917a2a815b1829e978
Daniel <Danielmayneps44@gmail.com>
2020-09-17 18:29:10 -0300

    suporte

---
+++ suporte.php
@@ -0,0 +1,18 @@
+<h1><b><p align="center">FALE CONOSCO </h1></b></p>
+    <p><b>E-mail : </b> suporte@gdan.com </b> </p>
+    <p><b>Instagram : </b> <instagram class="com">instagram.com/@gdan</p>
+       <b>Dúvidas ou Sugestões : </b>
+    <form>
+        <textarea name="critica" rowns="8" cols="80">Digite aqui...</textarea>
+        <br>
+        <input type="submit" value="Enviar" name="b1">
+        <input type="reset" value="Limpar" name="b2"></p>
+    </form>
+    <br>
+    <h2><b>COLABORADORES </h2></b></p>
+    <p><b>Fabiano Borgem Victor </p></b>
+    <p><b>Daniel Mayne Bento </p></b>
+    <p><b>Rolmes Antônio Junior </p></b>
+    <p><b>Lucas Rodrigues da Silva</p></b>
+    <p><b>Claudio Roberto de Brito</p></b>
+ 
